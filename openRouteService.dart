4eef67862af3a48092975bb6477e56d53b054588
Robert Sun <107655677+rsun19@users.noreply.github.com>
2023-01-06 02:58:24 -0500

    Update openRouteService.dart

--- openRouteService.dart
+++ openRouteService.dart
@@ -36,6 +36,7 @@ class ORSCaller {
 }
 
 Future getJsonData(ORSCaller orsCaller) async {
+  directions.clear();
   ORSCaller directionsInfo = ORSCaller(
       latStart: orsCaller.latStart,
       longStart: orsCaller.longStart,
@@ -47,18 +48,35 @@ Future getJsonData(ORSCaller orsCaller) async {
     print(data);
     Directions line =
         Directions(data['features'][0]['geometry']['coordinates']);
+    NavigtionDirections turns =
+        NavigtionDirections(data['features'][0]['properties']['segments']);
+    directions.add(turns.turns);
+    print(directions.length);
+    print(directions.toString());
     for (int i = 0; i < line.directions.length; i++) {
       points.add(LatLng(line.directions[i][1], line.directions[i][0]));
     }
+    points.add(LatLng(locationCoordinates[2], locationCoordinates[3]));
   } catch (e) {
     print(e);
   }
 }
 
+List<dynamic> directions = [];
+//access distance (seconds): directions[0]['distance']
+//access duration (meters): directions[0]['duration']
+//access steps: directions[0]['steps']
+
 class Directions {
   Directions(this.directions);
   List<dynamic> directions;
 }
+
+class NavigtionDirections {
+  NavigtionDirections(this.turns);
+  List<dynamic> turns;
+}
+
 
 class CoordinatesHelper {
   CoordinatesHelper({required this.area});
